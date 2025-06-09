66a4f130969cc293a3fa3ec35b518abf34178cfd
ariane-s-f16 <arianedesouzaficho@gmail.com>
2025-06-09 15:55:38 -0300

    foi

---
+++ autoload.php
@@ -0,0 +1,14 @@
+
+<?php
+spl_autoload_register(function($class) {
+    $baseDir = __DIR__ . '/';
+    $file = $baseDir . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+    $core = new Core;
+    $core-> start ($_GET);
+   
+?>
